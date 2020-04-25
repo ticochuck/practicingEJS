5057cd1624443c98c1bc50d167104a99c5a9ae75
ticochuck <chucklivilla@gmail.com>
2020-04-24 23:14:54 -0700

    changes

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const expressLayout =  require('express-ejs-layouts');
 
 const app = express();
 
 app.use(bodyParser());
 app.use(cors());
+app.use(expressLayout);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,6 +22,10 @@ app.get('/', function(request, response) {
       {name: 'laura'},
     ]
   });
+});
+
+app.get('/about', function(request, response) {
+  response.render('about');
 });
 
 app.listen(8000, function() {
